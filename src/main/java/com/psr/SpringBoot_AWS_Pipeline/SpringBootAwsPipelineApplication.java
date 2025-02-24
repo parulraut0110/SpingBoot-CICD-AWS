ee198983fe11ced40647b30b71b212645070d020
PARUL SURESH RAUT <parulraut0110@gmail.com>
2025-02-25 00:38:21 +0530

    Initial commit

---
+++ src/main/java/com/psr/SpringBoot_AWS_Pipeline/SpringBootAwsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.psr.SpringBoot_AWS_Pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsPipelineApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/psr/SpringBoot_AWS_Pipeline/api/HelloWorldApi.java
@@ -0,0 +1,13 @@
+package com.psr.SpringBoot_AWS_Pipeline.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldApi {
+	@GetMapping("/hello")
+	public String greetings() {
+		return "Hello World";
+	}
+
+}
